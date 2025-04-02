b6289a25e86d9e82983ceec057d108363f24b554
YongCheol Shin <kah3721@naver.com>
2025-04-03 03:06:23 +0900

    포스팅 공유하기 버튼 추가

--- assets/js/search.js
+++ assets/js/search.js
@@ -9,4 +9,36 @@ $(document).ready(function () {
         $(".search-content input").focus();
     }, 400);
   });
+
+  // $(".shareToggle").on("click", function () {
+  //   if($(this).hasClass('open')) {
+  //     $(this).removeClass('open');
+  //   } else {
+  //     $(this).removeClass('open');
+  //     $(this).addClass('open')
+  //   }
+  // });
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+  const page__share = document.querySelector('.page__share');
+  const shareToggle = document.querySelector('.shareToggle');
+  console.log(page__share)
+  shareToggle.addEventListener('click', function() {
+    if(!page__share.classList.contains('open')) {
+      page__share.classList.add('open');
+    } else {
+      page__share.classList.remove('open');
+    }
+  })
+  
+  document.addEventListener('click', function(e) {
+    if(!page__share.contains(e.target)) {
+      page__share.classList.remove('open');
+    }
+  });
+  
+  page__share.addEventListener('click', function(e) {
+    e.stopPropagation();
+  });
 })
