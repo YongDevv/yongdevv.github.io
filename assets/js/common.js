95c540ea61eba751137f31a12e8cc8f5bbd7aab0
YongCheol Shin <kah3721@naver.com>
2026-02-09 16:55:19 +0900

    레이아웃 수정(3)

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,28 @@
+// 프로필 링크 리스트 기능
 function toggleLinks() {
-    const links = document.getElementById("social-links");
-    links.classList.toggle("hidden");
+  const links = document.getElementById("social-links");
+  links.classList.toggle("hidden");
+}
+
+// 게시글 공유버튼 기능
+function toggleShare() {
+  const pageShare = document.querySelector('.page-share');
+  const shareToggle = document.querySelector('.share-toggle');
+  shareToggle.addEventListener('click', function() {
+    if(!pageShare.classList.contains('open')) {
+    pageShare.classList.add('open');
+    } else {
+    pageShare.classList.remove('open');
+    }
+  })
+
+  document.addEventListener('click', function(e) {
+    if(!pageShare.contains(e.target)) {
+    pageShare.classList.remove('open');
+    }
+  });
+
+  pageShare.addEventListener('click', function(e) {
+    e.stopPropagation();
+  });
 }
--- assets/js/search.js
+++ assets/js/search.js
@@ -1,27 +1,3 @@
-function toggleShare() {
-  // 게시글 공유버튼 기능
-  const pageShare = document.querySelector('.page-share');
-  const shareToggle = document.querySelector('.share-toggle');
-  shareToggle.addEventListener('click', function() {
-    if(!pageShare.classList.contains('open')) {
-      pageShare.classList.add('open');
-    } else {
-      pageShare.classList.remove('open');
-    }
-  })
-  
-  document.addEventListener('click', function(e) {
-    if(!pageShare.contains(e.target)) {
-      pageShare.classList.remove('open');
-    }
-  });
-  
-  pageShare.addEventListener('click', function(e) {
-    e.stopPropagation();
-  });
-}
-
-
 $(document).ready(function () {
   // Search toggle
   $(".search-toggle").on("click", function () {
